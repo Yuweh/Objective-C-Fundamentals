33a09d674761c77b33138f80bc7d6d77b3a0e772
Francis Jemuel Bergonia <Yuweh@users.noreply.github.com>
2017-10-08 12:19:11 +0800

    00-Class Anatomy
    Header and Implementation File + Properties and Methods

--- ViewController.h
+++ ViewController.h
@@ -6,10 +6,14 @@
 //  Copyright © 2017 Francis Jemuel Bergonia. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+#import <UIKit/UIKit.h> //imports the UIKIT classes and properties, including UIViewController
 
+@interface ViewController : UIViewController //this where your class name is named and inherits properties from UIViewController
+
+@property (nonatomic, strong) NSString *name; //anything in between @interface and @end which are placed ViewController.Header will be publicly available
+//the @property above could be accessed at ViewController.m
 
 @end
+
 
