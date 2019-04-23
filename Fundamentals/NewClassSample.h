18421888049247ba45c7b2d8d229fd02078b4e0d
Jay <Yuweh@users.noreply.github.com>
2019-04-23 10:04:06 +0800

    (っ˘▽˘)っ updated

---
+++ Fundamentals/NewClassSample.h
@@ -0,0 +1,13 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+{
+//add or drag ui elements here
+    IBOutlet UIView *mainView;
+    IBOutlet UIView *mainSubViewLayer;
+    IBOutlet UILabel *headTitleLabel;
+}
+
+@end
