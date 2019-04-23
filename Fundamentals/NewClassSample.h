189da0aaa8b4ef6fcc92b2a114b05b8a6b9c1bcd
Jay <Yuweh@users.noreply.github.com>
2019-04-23 10:07:07 +0800

    updated ⊂(◕.◕⊂)

--- Fundamentals/NewClassSample.h
+++ Fundamentals/NewClassSample.h
@@ -11,3 +11,54 @@
 }
 
 @end
+
+    
+    
+// for .m file
+    
+#import "HomeViewController.h"
+
+@implementation HomeViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    mainSubViewLayer.layer.cornerRadius = 5;
+    mainSubViewLayer.layer.masksToBounds = true;    
+}
+
+@end
+    
+//to use xib file as initial viewController
+    
+    @AppDelegate.h
+   
+#import <UIKit/UIKit.h>
+#import "HomeViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) HomeViewController *homeViewConroller;
+
+@end
+    
+    
+    
+    @AppDelegate.m
+    
+    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.homeViewConroller = [[HomeViewController alloc] initWithNibName:@"HomeViewController" bundle:nil];
+    //UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:self.homeViewConroller];
+    self.window.rootViewController = self.homeViewConroller;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+    
